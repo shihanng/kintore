854b75f76ce84a8c81ab9a834e1edd6297f1f887
Shi Han NG <shihanng@gmail.com>
2022-08-06 10:02:50 +0900

    exercism/elixir/freelancer-rates: Refactor

--- exercism/elixir/freelancer-rates/lib/freelancer_rates.ex
+++ exercism/elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,6 +1,9 @@
 defmodule FreelancerRates do
+  @daily_rate 8.0
+  @billable_days 22
+
   def daily_rate(hourly_rate) do
-    hourly_rate * 8.0
+    hourly_rate * @daily_rate
   end
 
   def apply_discount(before_discount, discount) do
@@ -8,7 +11,7 @@ defmodule FreelancerRates do
   end
 
   def monthly_rate(hourly_rate, discount) do
-    (daily_rate_after_discount(hourly_rate, discount) * 22) |> Float.ceil(0) |> trunc
+    (daily_rate_after_discount(hourly_rate, discount) * @billable_days) |> Float.ceil(0) |> trunc
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
