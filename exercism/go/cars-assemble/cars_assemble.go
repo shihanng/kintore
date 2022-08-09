f59ea719a77cf1566acef9181615602cd1ababc7
Shi Han NG <shihanng@gmail.com>
2022-08-09 17:12:54 +0900

    exercism/go/cars-assemble: Add solution

---
+++ exercism/go/cars-assemble/cars_assemble.go
@@ -0,0 +1,26 @@
+package cars
+
+const (
+	costPerCar   = 10000
+	costPer10Car = 95000
+)
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * successRate / 100
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	numBatch := carsCount / 10
+	remainingCars := carsCount - numBatch*10
+
+	return uint(numBatch*costPer10Car + remainingCars*costPerCar)
+}
