ea33d10152d0577c22cda43f0e0dc85273cf725d
Shi Han NG <shihanng@gmail.com>
2022-08-19 19:55:45 +0900

    exercism/elixir/rpg-character-sheet: Add solution

---
+++ exercism/elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -0,0 +1,33 @@
+defmodule RPG.CharacterSheet do
+  def welcome() do
+    IO.puts("Welcome! Let's fill out your character sheet together.")
+    :ok
+  end
+
+  def ask_name() do
+    IO.gets("What is your character's name?\n") |> String.trim()
+  end
+
+  def ask_class() do
+    IO.gets("What is your character's class?\n") |> String.trim()
+  end
+
+  def ask_level() do
+    {i, ""} =
+      IO.gets("What is your character's level?\n")
+      |> String.trim()
+      |> Integer.parse()
+
+    i
+  end
+
+  def run() do
+    welcome()
+    name = ask_name()
+    class = ask_class()
+    level = ask_level()
+    character = %{class: class, level: level, name: name}
+    IO.puts("Your character: #{inspect(character)}")
+    character
+  end
+end
