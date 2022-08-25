1ef510c5060b99abbc4bcc431027d1e4a3c91651
Shi Han NG <shihanng@gmail.com>
2022-08-26 08:29:40 +0900

    exercism/elixir/basketball-website: Add solution

---
+++ exercism/elixir/basketball-website/lib/basketball_website.ex
@@ -0,0 +1,17 @@
+defmodule BasketballWebsite do
+  def extract_from_path(data, path) do
+    do_extract_from_path(data, String.split(path, "."))
+  end
+
+  defp do_extract_from_path(data, []) do
+    data
+  end
+
+  defp do_extract_from_path(data, [head | tail]) do
+    do_extract_from_path(data[head], tail)
+  end
+
+  def get_in_path(data, path) do
+    Kernel.get_in(data, String.split(path, "."))
+  end
+end
