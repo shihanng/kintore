df939b3e5beaa1d16ffea3ee80341f754a7b752e
Shi Han NG <shihanng@gmail.com>
2022-08-27 09:49:05 +0900

    exercism/elixir/boutique-inventory: Add solution

---
+++ exercism/elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -0,0 +1,31 @@
+defmodule BoutiqueInventory do
+  def sort_by_price(inventory) do
+    Enum.sort(inventory, &(&1.price <= &2.price))
+  end
+
+  def with_missing_price(inventory) do
+    Enum.filter(inventory, &is_nil(&1.price))
+  end
+
+  def update_names(inventory, old_word, new_word) do
+    Enum.map(inventory, fn item ->
+      %{item | name: String.replace(item.name, old_word, new_word)}
+    end)
+  end
+
+  def increase_quantity(item, count) do
+    quantity_by_size =
+      Enum.map(item.quantity_by_size, fn {size, quantity} ->
+        {size, quantity + count}
+      end)
+      |> Enum.into(%{})
+
+    %{item | quantity_by_size: quantity_by_size}
+  end
+
+  def total_quantity(item) do
+    Enum.reduce(item.quantity_by_size, 0, fn {_size, quantity}, total ->
+      total + quantity
+    end)
+  end
+end
