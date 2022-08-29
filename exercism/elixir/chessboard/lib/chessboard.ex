f1ce1ebdff82bd53c97a1cbd9bb75511cc2d3ffb
Shi Han NG <shihanng@gmail.com>
2022-08-29 21:47:27 +0900

    exercism/elixir/chessboard: Add solution

---
+++ exercism/elixir/chessboard/lib/chessboard.ex
@@ -0,0 +1,17 @@
+defmodule Chessboard do
+  def rank_range do
+    1..8
+  end
+
+  def file_range do
+    ?A..?H
+  end
+
+  def ranks do
+    rank_range() |> Enum.to_list()
+  end
+
+  def files do
+    file_range() |> Enum.to_list() |> Enum.map(&List.to_string([&1]))
+  end
+end
