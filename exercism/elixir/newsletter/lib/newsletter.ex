b9510a2d236e932ec08667fa1e8aa25a42715aa2
Shi Han NG <shihanng@gmail.com>
2022-09-05 22:25:29 +0900

    exercism/elixir/newsletter: Add solution

---
+++ exercism/elixir/newsletter/lib/newsletter.ex
@@ -0,0 +1,36 @@
+defmodule Newsletter do
+  def read_emails(path) do
+    {:ok, binary} = File.read(path)
+    binary |> String.split()
+  end
+
+  def open_log(path) do
+    {:ok, file} = File.open(path, [:write])
+    file
+  end
+
+  def log_sent_email(pid, email) do
+    IO.binwrite(pid, email <> "\n")
+  end
+
+  def close_log(pid) do
+    File.close(pid)
+  end
+
+  def send_newsletter(emails_path, log_path, send_fun) do
+    log = open_log(log_path)
+
+    read_emails(emails_path)
+    |> Enum.each(fn email ->
+      case send_fun.(email) do
+        :ok ->
+          log_sent_email(log, email)
+
+        _ ->
+          nil
+      end
+    end)
+
+    close_log(log)
+  end
+end
