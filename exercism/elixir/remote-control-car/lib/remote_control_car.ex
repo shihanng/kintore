512d67e7be93b6cc40d20c69d4e647d8d89804e6
Shi Han NG <shihanng@gmail.com>
2022-09-06 21:26:35 +0900

    exercism/elixir/remote-control-car: Use default

--- exercism/elixir/remote-control-car/lib/remote_control_car.ex
+++ exercism/elixir/remote-control-car/lib/remote_control_car.ex
@@ -2,11 +2,7 @@ defmodule RemoteControlCar do
   @enforce_keys [:nickname]
   defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
 
-  def new() do
-    new("none")
-  end
-
-  def new(nickname) do
+  def new(nickname \\ "none") do
     %RemoteControlCar{nickname: nickname}
   end
 
