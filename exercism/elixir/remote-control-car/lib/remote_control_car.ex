5e0e5ebb98f2fe349b0e6c25ee856bc1a66f518f
Shi Han NG <shihanng@gmail.com>
2022-09-06 21:25:09 +0900

    exercism/elixir/remote-control-car: Add solution

---
+++ exercism/elixir/remote-control-car/lib/remote_control_car.ex
@@ -0,0 +1,34 @@
+defmodule RemoteControlCar do
+  @enforce_keys [:nickname]
+  defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
+
+  def new() do
+    new("none")
+  end
+
+  def new(nickname) do
+    %RemoteControlCar{nickname: nickname}
+  end
+
+  def display_distance(%RemoteControlCar{distance_driven_in_meters: dist}) do
+    "#{dist} meters"
+  end
+
+  def display_battery(%RemoteControlCar{battery_percentage: 0}) do
+    "Battery empty"
+  end
+
+  def display_battery(%RemoteControlCar{battery_percentage: bat}) do
+    "Battery at #{bat}%"
+  end
+
+  def drive(%RemoteControlCar{battery_percentage: 0} = remote_car) do
+    remote_car
+  end
+
+  def drive(
+        %RemoteControlCar{battery_percentage: bat, distance_driven_in_meters: dist} = remote_car
+      ) do
+    %{remote_car | battery_percentage: bat - 1, distance_driven_in_meters: dist + 20}
+  end
+end
