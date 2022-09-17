285bc8a395499456dd2039b0e83f932f63b94157
Shi Han NG <shihanng@gmail.com>
2022-09-17 16:11:53 +0900

    dynamic_prog/python/fibonacci: Memo

--- dynamic_prog/python/fibonacci/fibonacci.py
+++ dynamic_prog/python/fibonacci/fibonacci.py
@@ -3,3 +3,15 @@ def fib(n: int):
         return 1
 
     return fib(n - 1) + fib(n - 2)
+
+
+def fib_memo(n: int, memo={}):
+    val = memo.get(n)
+    if val:
+        return val
+
+    if n <= 2:
+        return 1
+
+    memo[n] = fib_memo(n - 1, memo) + fib_memo(n - 2, memo)
+    return memo[n]
--- dynamic_prog/python/fibonacci/test_fibonacci.py
+++ dynamic_prog/python/fibonacci/test_fibonacci.py
@@ -1,13 +1,29 @@
 import pytest
-from fibonacci import fib
+from fibonacci import fib, fib_memo
 
 
 @pytest.mark.parametrize(
     "n,expected",
     [
         (6, 8),
+        (7, 13),
+        (8, 21),
     ],
 )
 def test_fib(n, expected):
     actual = fib(n)
+    assert expected == actual
+
+    actual = fib_memo(n)
+    assert expected == actual
+
+
+@pytest.mark.parametrize(
+    "n,expected",
+    [
+        (50, 12586269025),
+    ],
+)
+def test_fib_memo(n, expected):
+    actual = fib_memo(n)
     assert expected == actual
