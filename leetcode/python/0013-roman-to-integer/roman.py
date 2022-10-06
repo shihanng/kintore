580e9c9ab913d77bfdfeef6244b58b37f7d897cc
Shi Han NG <shihanng@gmail.com>
2022-10-06 20:51:37 +0900

    leetcode/python/0013-roman-to-integer: Refactor

--- leetcode/python/0013-roman-to-integer/roman.py
+++ leetcode/python/0013-roman-to-integer/roman.py
@@ -10,23 +10,17 @@ class Solution:
     }
 
     def romanToInt(self, s: str) -> int:
-        sum, i = 0, 0
+        sum = 0
 
-        while i < len(s):
-            char = s[i]
-            next = s[i + 1] if i + 1 < len(s) else None
+        for j in range(len(s) - 1):
+            curr = self.symbols[s[j]]
+            next = self.symbols[s[j + 1]]
 
-            if char == "I" and (next == "V" or next == "X"):
-                sum += self.symbols[next] - self.symbols[char]
-                i += 2
-            elif char == "X" and (next == "L" or next == "C"):
-                sum += self.symbols[next] - self.symbols[char]
-                i += 2
-            elif char == "C" and (next == "D" or next == "M"):
-                sum += self.symbols[next] - self.symbols[char]
-                i += 2
+            if curr < next:
+                sum -= curr
             else:
-                sum += self.symbols[char]
-                i += 1
+                sum += curr
+
+        sum += self.symbols[s[-1]]
 
         return sum
