5ace0f95811ff165178443b8da0646c525c2e9d2
Shi Han NG <shihanng@gmail.com>
2022-10-12 19:55:47 +0900

    exercism/lua/diamond: Remove garbage

--- exercism/lua/diamond/diamond.lua
+++ exercism/lua/diamond/diamond.lua
@@ -22,24 +22,3 @@ return function(which)
 
 	return res
 end
-
--- 1 0 A
--- 2 1 A
--- 3 2 A
-
--- 5 4 A
---   3 B 0
---   2 C 1
---   1 D 2
---   0 E 3
-
--- 2 B 1
--- 3 C 3
--- 4 D 5
--- 5 E 7
-
---  A
--- B B
---  A
---
---  len*2 -1
