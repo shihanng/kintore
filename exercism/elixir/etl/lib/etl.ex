1da2b56ea49a98b17c80004bcee123eb7f7a165b
Shi Han NG <shihanng@gmail.com>
2022-10-21 19:51:47 +0900

    exercism/elixir/etl: Add solution

---
+++ exercism/elixir/etl/lib/etl.ex
@@ -0,0 +1,18 @@
+defmodule ETL do
+  @doc """
+  Transforms an old Scrabble score system to a new one.
+
+  ## Examples
+
+    iex> ETL.transform(%{1 => ["A", "E"], 2 => ["D", "G"]})
+    %{"a" => 1, "d" => 2, "e" => 1, "g" => 2}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+    Enum.reduce(input, %{}, fn {freq, chars}, acc ->
+      Enum.reduce(chars, acc, fn char, acc ->
+        Map.put(acc, String.downcase(char), freq)
+      end)
+    end)
+  end
+end
