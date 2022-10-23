412776577eb0d717fc67e13d297d5fd932d0badc
Shi Han NG <shihanng@gmail.com>
2022-10-23 22:10:08 +0900

    exercism/elixir/triangle: Add solution

---
+++ exercism/elixir/triangle/lib/triangle.ex
@@ -0,0 +1,25 @@
+defmodule Triangle do
+  @type kind :: :equilateral | :isosceles | :scalene
+
+  @doc """
+  Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
+  """
+  @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
+  def kind(a, b, c) when a <= 0 or b <= 0 or c <= 0 do
+    {:error, "all side lengths must be positive"}
+  end
+
+  def kind(a, b, c) when not (a + b >= c and b + c >= a and a + c >= b) do
+    {:error, "side lengths violate triangle inequality"}
+  end
+
+  def kind(a, b, c) when a == b and b == c do
+    {:ok, :equilateral}
+  end
+
+  def kind(a, b, c) when a == b or b == c or c == a do
+    {:ok, :isosceles}
+  end
+
+  def kind(_a, _b, _c), do: {:ok, :scalene}
+end
